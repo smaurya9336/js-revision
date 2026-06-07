04de5e1699aa0822976c48b0b17fbf5b9b141040
Sarita Maurya <sm7767119@gmail.com>
2026-06-07 22:24:28 +0530

    js new series

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+const heros=["saktiman","naagraj","doga"]
+
+let myObj={
+    name:"Sarita",
+    age:23,
+    city:"Delhi"
+}
+
+const myfunction=function (){
+    console.log("This is a function")
+}
+
+myfunction()
