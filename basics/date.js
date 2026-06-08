4fedebd1d96d8dfc0b13123274b28cf635d05f07
sarita maurya <sm7767119@gmail.com>
2026-06-08 12:10:19 +0530

    date and time in js

---
+++ basics/date.js
@@ -0,0 +1,24 @@
+// let myDate=new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.getFullYear())
+// console.log(myDate.getMonth() +1)
+// console.log(myDate.getDate())
+// console.log(myDate.getHours())
+// console.log(myDate.getMinutes())
+// console.log(myDate.getSeconds())
+
+// const createMyDate="2023-06-15"
+// console.log(new Date(createMyDate).toString())
+// // const createMyDate2="June 15, 2023"
+// const createMyDate2=new Date(2022,0,5,15,9)
+// console.log(createMyDate2.toLocaleString())
+// console.log(new Date(createMyDate2).toString())
+// const createMyDate3=1686806400000
+// console.log(new Date(createMyDate3).toString())
+
+console.log(Date.now())
+
+let newDate=new Date()
+console.log(newDate.getTime())
